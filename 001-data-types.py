05d8ff19fea781b493de60fcfa02b286d35e56d0
Joseph Jack Valverde Cipriano <89861723+valverdejoseph@users.noreply.github.com>
2022-05-17 21:18:22 -0500

    Data Types and Typescasting
    Int, str, float and bool.

---
+++ 001-data-types.py
@@ -0,0 +1,37 @@
+#This is a comment, you must add the hash symbol (#) before.
+
+#Data types
+#Integers     : int
+#Real numbers : float
+#Words        : str
+#Booleans     : bool
+
+#To see the actual data type
+type(11)
+#int
+type(21.20)
+#float
+type("Hi")
+#str
+type(True)
+#bool
+
+#To change the type of the expression (typecasting)
+float(2)
+#2.0
+int(1.1)
+#1
+int("1")
+#1
+str(1)
+#"1"
+str(4.5)
+#'4.5'
+int(True)
+#1
+int(False)
+#0
+bool(1)
+#True
+bool(0)
+#False
