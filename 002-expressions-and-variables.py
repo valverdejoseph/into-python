775dbbc086eb99f5c387317dc72b3853484fd963
Joseph Jack Valverde Cipriano <89861723+valverdejoseph@users.noreply.github.com>
2022-05-19 21:43:09 -0500

    Expressions and Variables
    Expressions with basic mathematical operations and definition of variables.

--- 002-expressions-and-variables.py
+++ 002-expressions-and-variables.py
@@ -1,1 +1,16 @@
 #Expressions: Operations the computer perform
+#Python follows mathematical conventions
+
+#Addition: x + y
+#Subtraction: x - y
+#Multiplication: x * y
+#Division: x / y, this result in a float
+#Integer Division: x // y, this result in a rounded int to the nearest integer
+
+
+#Variables: Memory spaces to store values
+my_var = 1
+x = 1 + 2 + 3 + 4
+y = x / 9
+type(y)
+#float
