0f9d822fa407c0579bf7b1261fa86a8754c495f4
Joseph Jack Valverde Cipriano <89861723+valverdejoseph@users.noreply.github.com>
2022-05-23 17:16:46 -0500

    Fundamental Data Types Definition

--- 001-data_types.py
+++ 001-data_types.py
@@ -1,4 +1,4 @@
-#This is a comment, you must add the hash symbol (#) before.
+#This is a comment, you must add the hash symbol (#) before
 
 #Data types
 #Integers     : int
@@ -6,34 +6,21 @@
 #Words        : str
 #Booleans     : bool
 
-#To see the actual data type
-type(11)
-#int
-type(21.20)
-#float
-type("Hi")
-#str
-type(True)
-#bool
+#To see the actual data type:
+type(11) #int
+type(21.20) #float
+type("Hi") #str
+type(True) #bool
 
-#To change the type of the expression (typecasting)
-float(2)
-#2.0
-int(1.1)
-#1
-int("1")
-#1
-str(1)
-#"1"
-str(4.5)
-#'4.5'
-int(True)
-#1
-int(False)
-#0
-bool(1)
-#True
-bool(0)
-#False
+#To change the type of the expression (typecasting):
+float(2) #2.0
+int(1.1) #1
+int("1") #1
+str(1) #"1"
+str(4.5) #'4.5'
+int(True) #1
+int(False) #0
+bool(1) #True
+bool(0) #False
 
 #Note: strings can be represented with single quotes ('1.0') or double quotes ("2.0"), but you can't mix both ("1.0')
