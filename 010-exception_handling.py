3d24c2e95e536fcef26cb4578d5dfa74d70d0d81
Joseph Jack Valverde Cipriano <89861723+valverdejoseph@users.noreply.github.com>
2022-06-14 17:03:52 -0500

    The Basics of Try - Exception

--- 010-exception_handling.py
+++ 010-exception_handling.py
@@ -7,6 +7,8 @@ try:
   file.write('My file of mine.')
 except IOError:
   print('Unable to open or read the data in the file.')
+except (ZeroDivisionError, NameError):
+  print('Ok, try again!')
 except: #For errors that aren't specified. This isn't a best practice
   print('Some other error occurred!')
 else: #If no errors occurs
@@ -14,3 +16,10 @@ else: #If no errors occurs
 finally: #To continue execution regardless of the end result
   file.close()
   print('File is now closed.')
+#Code outside the Try block will continue execution
+#Error:
+#ZeroDivisionError, when divided by zero
+#NameError, when a variable is not defined
+#IndexError, when no index exists for a data type
+#More:
+#https://docs.python.org/3/library/exceptions.html?utm_medium=Exinfluencer&utm_source=Exinfluencer&utm_content=000026UJ&utm_term=10006555&utm_id=NA-SkillsNetwork-Channel-SkillsNetworkCoursesIBMDeveloperSkillsNetworkPY0101ENSkillsNetwork19487395-2021-01-01
