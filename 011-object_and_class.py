e00b07be1e6a9a0f36bc90e5839c538679704b32
Joseph Jack Valverde Cipriano <89861723+valverdejoseph@users.noreply.github.com>
2022-06-15 11:14:24 -0500

    The Definitions of Instance, Object and Class

--- 011-object_and_class.py
+++ 011-object_and_class.py
@@ -23,6 +23,58 @@ class Book(object): #Class definition, Name, and Parent of the class
     #title and pages are parameters of the newly created instance
     self.title=title
     self.pages=pages
+  #Next, the definition of methods, which are functions to interact and
+  #change the data attributes, changing or using these
+  #To know how many pages are left to read:
+  def remainPages(self,pagesRead=0):
+    return self.pages - pagesRead
+  #To get the title length:
+  def titleLen(self):
+    return len(self.title)
+  #To capitalize the title:
+  def upTitle(self):
+    self.title=self.title.upper()
 #To create an instance:
-aBook=Book('The Lord of the Rings',1200)
+#Name of object=Name of class(Attributes)
+aBook=Book('The Lord of the Rings',1200) 
 bBook=Book('The Bible',10000)
+#To get a data attribute, .dataAttribute:
+aBook.title #'The Lord of the Rings'
+bBook.pages #10000
+#To change a data attribute, .dataAttribute:
+aBook.title='Oedipus Rex'
+aBook.title #'Oedipus Rex'
+#To test the methods:
+aBook.remainPages(500) #700
+aBook.titleLen() #21
+aBook.upTitle()
+aBook.title #'OEDIPUS REX'
+bBook.remainPages(10) #9990
+bBook.titleLen() #9
+bBook.upTitle()
+bBook.title #'THE BIBLE'
+#To get the list of data attributes and methods of a class, dir():
+dir(aBook)
+#['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__',
+#'__format__', '__ge__', '__getattribute__', '__gt__', '__hash__',
+#'__init__', '__init_subclass__', '__le__', '__lt__', '__module__',
+#'__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__',
+#'__setattr__', '__sizeof__', '__str__', '__subclasshook__',
+#'__weakref__', 'pages', 'remainPages', 'title', 'titleLen', 'upTitle']
+dir(int)
+#['__abs__', '__add__', '__and__', '__bool__', '__ceil__', '__class__',
+#'__delattr__', '__dir__', '__divmod__', '__doc__', '__eq__', '__float__',
+#'__floor__', '__floordiv__', '__format__', '__ge__', '__getattribute__',
+#'__getnewargs__', '__gt__', '__hash__', '__index__', '__init__',
+#'__init_subclass__', '__int__', '__invert__', '__le__', '__lshift__',
+#'__lt__', '__mod__', '__mul__', '__ne__', '__neg__', '__new__', '__or__',
+#'__pos__', '__pow__', '__radd__', '__rand__', '__rdivmod__',
+#'__reduce__', '__reduce_ex__', '__repr__', '__rfloordiv__',
+#'__rlshift__', '__rmod__', '__rmul__', '__ror__', '__round__',
+#'__rpow__', '__rrshift__', '__rshift__', '__rsub__', '__rtruediv__',
+#'__rxor__', '__setattr__', '__sizeof__', '__str__', '__sub__',
+#'__subclasshook__', '__truediv__', '__trunc__', '__xor__',
+#'as_integer_ratio', 'bit_length', 'conjugate', 'denominator',
+#'from_bytes', 'imag', 'numerator', 'real', 'to_bytes']
+#The attributes with underscores are for internal use, the others are
+#to be taken into account
