4173356584daf856c8b1996fbd7be0f9369a0252
Joseph Jack Valverde Cipriano <89861723+valverdejoseph@users.noreply.github.com>
2022-06-21 12:28:50 -0500

    Loading Data with Pandas Library

--- 013-loading_data.py
+++ 013-loading_data.py
@@ -8,6 +8,10 @@
 #Now, import the Pandas library in Python:
 import pandas as pd
 #'as' keyword gives an alias
+#Pandas generally provide two data structures for manipulating data:
+#-DataFrame, a two-dimensional data structure with rows and columns
+#-Series, a one-dimensional array of indexed data with actual data and indexes
+#A series as a one-dimensional dataframe
 #To read a csv file, pandas.read_csv():
 csv_path='file.csv'
 data=pd.read_csv(csv_path)
@@ -43,3 +47,26 @@ b_teams
 #            peru       españa
 #0  universitario  real madrid
 #1     binacional     espanyol
+#To select a specific row and column by their labels, .loc[]:
+d_teams.loc[0,'peru'] #'universitario'
+#To select a specific row and column by their indexes, .iloc[]:
+d_teams.iloc[0,1] #'real madrid'
+#To slicing with .loc():
+d_teams.loc[0:1,'peru':'españa']
+#            peru       españa
+#0  universitario  real madrid
+#1     binacional     espanyol
+#To slicing with .iloc():
+d_teams.iloc[0:2,0:1]
+#            peru
+#0  universitario
+#1     binacional
+#Note that the last index is not taken into account in .iloc[],
+#otherwise it's taken into account in .loc[]
+#To view the column as a series, just use one bracket:
+c_teams=d_teams['peru']
+c_teams
+#0    universitario
+#1       binacional
+#Name: peru, dtype: object
+type(c_teams) #<class 'pandas.core.series.Series'>
